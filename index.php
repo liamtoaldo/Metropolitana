05cdfb24431ddfee29f987f539dfeda5f4d946ff
Gyro7 <liamtoaldo@gmail.com>
2023-05-16 14:34:27 +0200

    Added login.php

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <!DOCTYPE HTML>
 <html>
+<title>Metropolitana - Login</title>
 <head>
+    <?php
+        $idUtente = NULL;
+        if(isset($_SESSION["idUtente"])) {
+            $idUtente = $_SESSION["idUtente"];
+        } else {
+            header('Location:login.php');
+        }
+
+    ?>
+
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogodownload.org%2Fwp-content%2Fuploads%2F2017%2F03%2Fas-roma-logo-0-1536x1536.png">
+
+    <!-- Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Import jQuery before materialize.js-->
+     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
---
+++ login.php
@@ -0,0 +1,158 @@
+<!DOCTYPE HTML>
+<html>
+<title>Metropolitana - Login</title>
+<head>
+    <style>
+        html, body, .my-wrapper {
+            height: 100%;
+        }
+        .backimg {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            z-index: -1;
+            filter: blur(5px);
+        }
+        .logo {
+            width: 40%;
+            height: 40%;
+        }
+        .card {
+            width:35vw;
+        }
+
+    </style>
+
+    <?php
+        if(isset($_GET[""]))
+    ?>
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogodownload.org%2Fwp-content%2Fuploads%2F2017%2F03%2Fas-roma-logo-0-1536x1536.png">
+
+    <!-- Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Import jQuery before materialize.js-->
+     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
+    <script>
+        
+        var themeColor ;
+
+        // Check browser support
+        if (typeof(Storage) !== "undefined") {
+            // Store
+            themeColor = localStorage.getItem("themeColor");
+        if(themeColor == undefined)
+        {localStorage.setItem("themeColor", "blue");
+            themeColor = 'blue';
+            } 
+        
+        } else {
+        Materialize.toast("Sorry, your browser does not support Web Storage...", 4000) 
+        }
+
+        $(".nav-wrapper").css("background-color", themeColor);
+        $(".secondary-content>.material-icons").css("color", themeColor);
+        $(".btn").css("background-color", themeColor);
+        $(".page-footer").css("background-color",themeColor);
+        $(".input-field").css("color", themeColor);
+        $(".input-field>.material-icons").css("color", themeColor);
+        $(".input-field>label").css("color",themeColor);
+        $(".dropdown-content>li>a").css("color", themeColor);
+
+        $(document).ready(function(){
+        
+        
+        $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrainWidth: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false, // Displays dropdown below the button
+            alignment: 'left', // Displays dropdown with edge aligned to the left of button
+            stopPropagation: false // Stops event propagation
+            }
+        );
+        var themeColor = "#822433";
+        $(".nav-wrapper").css("background-color", themeColor);
+        $(".secondary-content>.material-icons").css("color", themeColor);
+        $(".btn").css("background-color", themeColor);
+        $(".page-footer").css("background-color", themeColor);
+        $(".input-field").css("color", themeColor);
+        $(".input-field>.material-icons").css("color", themeColor);
+        $(".input-field>label").css("color", themeColor);
+        $(".btn-floating").css("background-color", themeColor);
+        $(".dropdown-content>li>a").css("color", themeColor);
+        
+        // Update Theme Color
+        if (typeof(Storage) !== "undefined") {
+            // Store
+            localStorage.setItem("themeColor", themeColor);
+
+        } else {
+            Materialize.toast("Sorry, your browser does not support Web Storage...", 4000) 
+        }
+
+        });
+</script>
+</head>
+<body class="dark">
+    <div class="my-wrapper container valign-wrapper">
+        <div class="row">
+            <!-- <div class="col s12 m6">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.metropolitanadiroma.it%2Fimages%2Fnotizie%2F2019-06%2Fmetro-b-stazione-repubblica-riapre.jpg" alt="Your image description" class="responsive-img">
+            </div> -->
+            <div class="col s12 m12">
+                <div class="card" style="border-radius: 15px">
+                    <div class="card-content center-align">
+                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ilprimatonazionale.it%2Fwp-content%2Fuploads%2F2014%2F12%2Fatac-logo.png" class="prefix logo">
+                        <span class="card-title"><h5><b>Metropolitana - Login</b></h5></span>
+                        <br>
+                        <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post">
+                            <div class="input-field">
+                                <i class="material-icons prefix">account_circle</i>
+                                <input id="username" type="text" class="validate" name="username">
+                                <label for="username">Username</label>
+                            </div>
+                            <div class="input-field">
+                                <i class="material-icons prefix">lock</i>
+                                <input id="password" type="password" class="validate" name="password">
+                                <label for="password">Password</label>
+                            </div>
+                            <div class="col s12 m12 l12">
+                                <label>
+                                    <input type="checkbox" class="filled-in" checked="checked" name="remember" value="yes"/>
+                                    <span>Ricordami</span>
+                                </label>
+                            </div>
+                            <br>
+                            <a style="border-radius: 15px" class="waves-effect waves-red btn-flat" href="register.php">Registrati
+                                <i class="material-icons right">person_add</i>
+                            </a>
+                            <button style="border-radius: 15px" class="btn waves-effect waves-light" type="submit" name="action">Login
+                                <i class="material-icons right">login</i>
+                            </button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <img class="backimg" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2Fdbjatwwqsyg41.jpg" alt="Background image">
+
+</body>
+
+</html>
